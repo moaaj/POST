f38e507a08b7061c063420a58bc144cc4697bf91
Abdullah Moaaj <67410953+moaaj@users.noreply.github.com>
2023-09-02 11:51:49 +0800

    Add files via upload

---
+++ Login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Student Info</h1>
+
+    <form action="POST.php" method="post">
+
+    <b>User Name:<b> <input type="text" name="username">
+    <b>Password<b> <input type="password" name="password">
+    <br>
+    <input type="submit">
+
+
+</form>
+    
+</body>
+</html>
---
+++ POST.php
@@ -0,0 +1,18 @@
+<?php
+
+
+
+
+if(($_POST["username"]==="mark") && ($_POST["password"]==="pass"))
+{
+    echo "username and password is correct";
+}
+else
+{
+    echo "username and password is not correct";
+}
+
+
+
+
+?>
